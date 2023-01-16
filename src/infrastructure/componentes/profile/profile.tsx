6d0fe21428533063d60cc94eb6761633a8169f08
Roberto <cacaracteres86@gmail.com>
2023-01-16 15:13:10 +0100

    Confirmacion para eliminar cuenta

--- src/infrastructure/componentes/profile/profile.tsx
+++ src/infrastructure/componentes/profile/profile.tsx
@@ -20,6 +20,26 @@ function Profile() {
             }
         });
     };
+    const onClicDeleteAccount = () => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'your account was successfully deleted.',
+                    'success'
+                );
+                handleDeleteAccount();
+            }
+        });
+    };
     return (
         <>
             {user.user?.purchasedProducts[0] ? (
@@ -32,7 +52,7 @@ function Profile() {
                             </a>
                             <button
                                 className={style.button}
-                                onClick={() => handleDeleteAccount()}
+                                onClick={() => onClicDeleteAccount()}
                             >
                                 delete Account
                             </button>
